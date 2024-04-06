8f5ed0a026747d842b88edeabd2b7a7f66fad112
TomasBozzano <tomycas10@gmail.com>
2024-04-06 20:14:59 -0300

    event 'Enter' for mobile and computer

--- src/js/api.js
+++ src/js/api.js
@@ -1,46 +1,74 @@
 document.addEventListener('DOMContentLoaded', async () => {
-	var kelvin = 273
 
 	const search = document.getElementById("search-button")
 	search.addEventListener('click', async (e) => {
+		e.preventDefault()
+		results()
+	})
 
-		e.preventDefault(); // Evitamos el comportamiento por defecto
-		const city = document.querySelector('[name=ciudad]').value
-		const dateSearch = document.getElementById('dateSearch')
-		city.innerHTML = ''
-		dateSearch.innerHTML = ''
-		obtenerDatosClima(city)
-			.then(data => {
-				const temp = data.main.temp - kelvin
-				const feels = data.main.feels_like - kelvin
-				const visibility = data.visibility/1000
-				const time = data.weather[0].main
-				let timeDay 
-				let tipo
-				const temperatura = Number(temp.toFixed(2))
-				const feel = Number(feels.toFixed(2))
-				const detail = document.createElement('summary')
-				/*
-				Clear = Cielo limpio o soleado
-				Clouds = Nublado
-				Rain = Lluvia
-				Drizzle = Lluvia
-				*/
-				if(time == 'Clear'){
-					 timeDay = 'sun-time.svg'
-					 tipo = 'Soleado'
-				}else if(time == 'Clouds'){
-					 timeDay = 'cloud-time.svg'
-					 tipo = 'Nublado'
-				}else if(time == 'Rain' || 'Drizzle'){
-					 timeDay = 'rain-time.svg'
-					 tipo = 'Lluvia'
-				} else{
-					 timeDay = 'wint.svg'
-					 tipo = 'Vientos'
-				};
+	const dato = document.querySelector('[name=ciudad]')
+	dato.addEventListener('keyup', (event) => {
+		if (event.key === 'Enter') {
+			results();
+		}
+	})
 
-				detail.innerHTML = `<div class="dateSearch">
+})
+
+function obtenerDatosClima(datosGet) {
+	const url = `https://weather-api138.p.rapidapi.com/weather?city_name=${datosGet}`
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'c2ace9d8d7mshaccd3c39a6765e8p1a1a31jsn48718f87c8c9',
+			'X-RapidAPI-Host': 'weather-api138.p.rapidapi.com'
+		}
+	};
+	return fetch(url, options)
+		.then(response => response.json())
+		.then(data => {
+			return data
+		})
+		.catch(error => {
+			console.error('Error al obtener datos del clima:', error)
+			throw error;
+		});
+}
+
+const results = async () => {
+	var kelvin = 273
+	const city = document.querySelector('[name=ciudad]').value
+	const dateSearch = document.getElementById('dateSearch')
+	city.innerHTML = ''
+	dateSearch.innerHTML = ''
+	if (city !== '') {
+		try {
+			const data = await obtenerDatosClima(city);
+			const temp = data.main.temp - kelvin;
+			const feels = data.main.feels_like - kelvin;
+			const visibility = data.visibility / 1000;
+			const time = data.weather[0].main;
+			let timeDay;
+			let tipo;
+			const temperatura = Number(temp.toFixed(2));
+			const feel = Number(feels.toFixed(2));
+
+			if (time === 'Clear') {
+				timeDay = 'sun-time.svg';
+				tipo = 'Soleado';
+			} else if (time === 'Clouds') {
+				timeDay = 'cloud-time.svg';
+				tipo = 'Nublado';
+			} else if (time === 'Rain' || time === 'Drizzle') {
+				timeDay = 'rain-time.svg';
+				tipo = 'Lluvia';
+			} else {
+				timeDay = 'wint.svg';
+				tipo = 'Vientos';
+			}
+
+			const detail = document.createElement('summary');
+			detail.innerHTML = `<div class="dateSearch">
 										<div class="timeDate">
 											<div class="infoCity">
 												<img src="src/image/map.svg" alt="Ubication">
@@ -84,32 +112,13 @@ document.addEventListener('DOMContentLoaded', async () => {
 												</tr>
 											</tbody>
 										</table>
-									</div>`
-				dateSearch.appendChild(detail);
-			})
-			.catch(error => {
-				alert('La ciudad que esta buscando, no se encuentra disponible. Escriba nuevamente una')
-				console.log(error)
-			})
-	})
-})
-
-function obtenerDatosClima(datosGet) {
-	const url = `https://weather-api138.p.rapidapi.com/weather?city_name=${datosGet}`
-	const options = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Key': 'c2ace9d8d7mshaccd3c39a6765e8p1a1a31jsn48718f87c8c9',
-			'X-RapidAPI-Host': 'weather-api138.p.rapidapi.com'
+									</div>`;
+			dateSearch.appendChild(detail);
+		} catch (error) {
+			alert('La ciudad que está buscando no se encuentra disponible. Por favor, escriba nuevamente una ciudad válida.');
+			console.error(error);
 		}
-	};
-	return fetch(url, options)
-		.then(response => response.json())
-		.then(data => {
-			return data
-		})
-		.catch(error => {
-			console.error('Error al obtener datos del clima:', error)
-			throw error;
-		});
-}
+	} else {
+		alert('Por favor, ingrese una ciudad válida.');
+	}
+};
